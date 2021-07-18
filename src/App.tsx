2c7d59f36d126b61a0a8beca3375ee43cb3e9289
João Henrique <joao.henrique-1998@hotmail.com>
2021-07-18 01:22:49 -0300

    Hooks encapsulado

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-
 import { BrowserRouter, Route } from 'react-router-dom'
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
+import { AuthContextProvider } from './contexts/AuthContext';
 
 
 
 
- function App() {
- 
 
+
+
+function App() {
+   
   return (
    <BrowserRouter>
-   <TestContext.Provider value={{ value, setValue }}>
+   <AuthContextProvider>
    <Route path="/" exact component={Home} />
    <Route path="/rooms/new" component={NewRoom} />
-   </TestContext.Provider>
+   </AuthContextProvider>
    </BrowserRouter>
   );
 }
